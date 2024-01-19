8fba96069f784b017e75434ea956038c08c3a47e
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2024-01-19 16:17:40 +0200

    Refactored projact by 'black'

--- cafe/apps.py
+++ cafe/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class CafeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cafe'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "cafe"
