a172ed0babdb3c158ab2d8692c5af5b1da1f4571
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2024-01-21 19:02:42 +0200

    Added User model to 'user' app

--- user/models.py
+++ user/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    User model with the '_group' method. This method is needed to display a
+    list of groups to which the user belongs in the Django admin panel.
+    """
+
+    def _groups(self):
+        return ",".join([str(p) for p in self.groups.all()])
