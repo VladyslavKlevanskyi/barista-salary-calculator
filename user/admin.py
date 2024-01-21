a0710600aa0f2b118098e3368cbe82d2d156964f
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2024-01-21 19:05:00 +0200

    Registered User model to admin.py in 'user' app

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "_groups",
+    )
