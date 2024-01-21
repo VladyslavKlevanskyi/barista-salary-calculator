541885fe3300e6a049ef7a9b92a2642814ad0ac4
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2024-01-21 17:17:16 +0200

    Added Cafe and Barista models in 'cafe' app

--- cafe/models.py
+++ cafe/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Cafe(models.Model):
+    """
+    Cafe model with name field.
+    """
+
+    name = models.CharField(max_length=63, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["id"]
+
+
+class Barista(models.Model):
+    """
+    Barista model with full_name field.
+    """
+
+    full_name = models.CharField(max_length=150, blank=False)
+
+    def __str__(self):
+        return self.full_name
+
+    class Meta:
+        ordering = ["id"]
