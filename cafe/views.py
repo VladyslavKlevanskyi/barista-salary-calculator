be12a34534cb5f76cb4acc6826618c29f78bc5b9
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2024-01-21 19:32:42 +0200

    Added Index view to 'cafe' app

--- cafe/views.py
+++ cafe/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from cafe.models import Cafe, Barista
+
+
+class Index(generic.TemplateView):
+    """
+    Index page displaying the total number of cafes and baristas.
+    """
+
+    template_name = "cafe/index.html"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        num_cafes = Cafe.objects.count()
+        num_baristas = Barista.objects.count()
+
+        context = {
+            "num_cafes": num_cafes,
+            "num_baristas": num_baristas,
+        }
+
+        return context
