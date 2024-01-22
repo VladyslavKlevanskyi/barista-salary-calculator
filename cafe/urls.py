91ae87d3938396749f00118bbfc8a400775e95cc
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2024-01-22 22:02:56 +0200

    Added url and shift_confirm_delete.html template for ShiftDeleteView to 'cafe' app

--- cafe/urls.py
+++ cafe/urls.py
@@ -4,6 +4,7 @@ from cafe.views import (
     ShiftListView,
     ShiftCreateView,
     ShiftUpdateView,
+    ShiftDeleteView,
 )
 
 urlpatterns = [
@@ -11,6 +12,7 @@ urlpatterns = [
     path("shifts/", ShiftListView.as_view(), name="shift-list-view"),
     path("shifts/create/", ShiftCreateView.as_view(), name="shift-create"),
     path("shifts/<int:pk>/update/", ShiftUpdateView.as_view(), name="shift-update"),
+    path("shifts/<int:pk>/delete/", ShiftDeleteView.as_view(), name="shift-delete"),
 ]
 
 app_name = "cafe"
