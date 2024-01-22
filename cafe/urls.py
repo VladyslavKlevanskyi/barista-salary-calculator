a4dcd0b935847633b2af7bf1f2fccf2c8bdf7d5c
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2024-01-22 18:25:13 +0200

    Added urls and shift_list.html template for ShiftListView to 'cafe' app

--- cafe/urls.py
+++ cafe/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from cafe.views import (
     Index,
+    ShiftListView,
 )
 
 urlpatterns = [
     path("", Index.as_view(), name="index"),
+    path("shifts/", ShiftListView.as_view(), name="shift-list-view"),
 ]
 
 app_name = "cafe"
