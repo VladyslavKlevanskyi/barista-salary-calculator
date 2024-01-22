021d43cc8fa03179a0b059d41e941a12d939fead
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2024-01-22 21:56:09 +0200

    Added url for ShiftUpdateView to 'cafe' app

--- cafe/urls.py
+++ cafe/urls.py
@@ -3,12 +3,14 @@ from cafe.views import (
     Index,
     ShiftListView,
     ShiftCreateView,
+    ShiftUpdateView,
 )
 
 urlpatterns = [
     path("", Index.as_view(), name="index"),
     path("shifts/", ShiftListView.as_view(), name="shift-list-view"),
     path("shifts/create/", ShiftCreateView.as_view(), name="shift-create"),
+    path("shifts/<int:pk>/update/", ShiftUpdateView.as_view(), name="shift-update"),
 ]
 
 app_name = "cafe"
