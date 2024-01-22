ff8011960f68a655521912637315c7bd50eac4d3
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2024-01-22 18:41:42 +0200

    Added urls and shift_form.html template for ShiftCreateView to 'cafe' app

--- cafe/urls.py
+++ cafe/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from cafe.views import (
     Index,
     ShiftListView,
+    ShiftCreateView,
 )
 
 urlpatterns = [
     path("", Index.as_view(), name="index"),
     path("shifts/", ShiftListView.as_view(), name="shift-list-view"),
+    path("shifts/create/", ShiftCreateView.as_view(), name="shift-create"),
 ]
 
 app_name = "cafe"
