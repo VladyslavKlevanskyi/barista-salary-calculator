b7302d9c03ebf75795acba9022528be377846912
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2024-01-22 18:38:29 +0200

    Added ShiftForm to 'cafe' app

--- cafe/forms.py
+++ cafe/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from cafe.models import Cafe, Barista, Shift
 
 
 class DateRangeForm(forms.Form):
@@ -16,3 +17,22 @@ class DateRangeForm(forms.Form):
         widget=forms.DateInput(attrs={"class": "datepicker"}),
         input_formats=["%Y-%m-%d"],
     )
+
+
+class ShiftForm(forms.ModelForm):
+    """
+    Form for creating a shift. You need to select a date, cafe and barista.
+    """
+
+    date = forms.DateField(
+        label="",
+        widget=forms.DateInput(attrs={"class": "datepicker"}),
+        input_formats=["%Y-%m-%d"],
+    )
+
+    cafe = forms.ModelChoiceField(queryset=Cafe.objects.all())
+    barista = forms.ModelChoiceField(queryset=Barista.objects.all())
+
+    class Meta:
+        model = Shift
+        fields = ["date", "cafe", "barista"]
