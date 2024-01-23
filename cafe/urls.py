99ef3dcd9be1f5f63e8b9b4cb87558fefa1d8469
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2024-01-23 17:10:08 +0200

    Added url and cafe_list.html template for CafeListView to 'cafe' app

--- cafe/urls.py
+++ cafe/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from cafe.views import (
     Index,
+    CafeListView,
     ShiftListView,
     ShiftCreateView,
     ShiftUpdateView,
@@ -9,6 +10,7 @@ from cafe.views import (
 
 urlpatterns = [
     path("", Index.as_view(), name="index"),
+    path("cafes/", CafeListView.as_view(), name="cafe-list-view"),
     path("shifts/", ShiftListView.as_view(), name="shift-list-view"),
     path("shifts/create/", ShiftCreateView.as_view(), name="shift-create"),
     path("shifts/<int:pk>/update/", ShiftUpdateView.as_view(), name="shift-update"),
